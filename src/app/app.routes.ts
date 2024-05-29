17a51eee481f53127c72b4718fd0a94557793abd
Zamoula <jamelmrad000@gmail.com>
2024-05-29 22:41:01 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { NotesListComponent } from './componenets/notes-list/notes-list.component';
+import { CreateNoteComponent } from './componenets/create-note/create-note.component';
+import { NoteDetailsComponent } from './componenets/note-details/note-details.component';
+import { PageNotFoundComponent } from './componenets/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: NotesListComponent
+    },
+    {
+        path: 'new',
+        component: CreateNoteComponent
+    },
+    {
+        path: ':id',
+        component: NoteDetailsComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
