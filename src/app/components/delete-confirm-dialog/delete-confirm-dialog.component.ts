6fe2599d067f2105bc8b5c8b17771eec479d2148
Zamoula <jamelmrad000@gmail.com>
2024-05-30 15:19:05 +0100

    dialog component to cofirm note deletion

---
+++ src/app/components/delete-confirm-dialog/delete-confirm-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirm-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-confirm-dialog.component.html',
+  styleUrl: './delete-confirm-dialog.component.css'
+})
+export class DeleteConfirmDialogComponent {
+
+}
